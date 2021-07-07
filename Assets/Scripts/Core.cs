d8dee34f79aa0d8b67f1f05bde0d21f082632668
Part_lo15 <jump-rostov@mail.ru>
2021-07-08 00:10:16 +0300

    обновил Сore

--- Assets/Scripts/Core.cs
+++ Assets/Scripts/Core.cs
@@ -1,24 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-//модель данных всего
-public class Core : MonoBehaviour
+//Общее хранилище данных. 
+[Serializable]
+public class Core 
 {
-    public class Price
-    {   string waterName;
-        int retailPrice;
-    }
+
+    //класс контрагента.
+    [Serializable]
     public class Kontragent
     {
-        string adress;
-        string name;
-        string Telephone;
+        string Adress {get; set;}
+        string name{ get; set;}
+        string Telephone { get; set;}
 
-        Dictionary<string, Price> prise = new Dictionary<string, Price>();
+        //список воды у контрагента ключ это название воды, значение это цена розничная воды
+        Dictionary<string, int> Price = new Dictionary<string, int>();
     }
 
+    //класс хранения данных для отчетов статистики
+    [Serializable]
+    public class Statistics
+    {
+        //выручка
+        int revenue;
+        // прибыль
+        int profit;
+        //словарь зарплат сотрудников        
+        Dictionary<string, int> peoplesSalaries = new Dictionary<string, int>();
+    }
+
+    //список контрагентов 
     public List<Kontragent> kontragents = new List<Kontragent>();
 
-    
+    //словарь номенклатуры ключ это название воды, значение это цена закупки воды
+    public Dictionary<string, int> nomenklatura = new Dictionary<string, int>();
+
+    //список маршрутных листов
+    public List<Transform> routeList = new List<Transform>();
+
+    //список водителей
+    public List<string> driverList = new List<string>();
+
+    public Statistics statistics;
+
 }
---
+++ Assets/Scripts/S.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+public class S : MonoBehaviour
+{
+    Core core = new Core();
+
+    public void seri()
+    {
+        core.kontragents.Add(new Core.Kontragent());
+
+        BinaryFormatter formatter = new BinaryFormatter();
+        using (FileStream fs = new FileStream("Core.dat", FileMode.OpenOrCreate))
+        {
+            formatter.Serialize(fs, core);
+        }
+        core = null;
+    }
+
+    public void desiri()
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        using (FileStream fs = new FileStream("Core.dat", FileMode.OpenOrCreate))
+        {
+            core = (Core)formatter.Deserialize(fs);
+        }
+
+    }
+}
